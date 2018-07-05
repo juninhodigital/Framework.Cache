8aa164d5b329e88cfa759ee11c48da6eaf0623ae
Jose Carlos de Jesus Junior <jjunior@edfmancapital.com>
2018-07-05 10:17:11 -0300

    Initial release

--- Framework.Cache/obj/Debug/netstandard2.0/Framework.Cache.AssemblyInfo.cs
+++ Framework.Cache/obj/Debug/netstandard2.0/Framework.Cache.AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Framework.Cache")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Framework.Cache")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Framework.Cache")]
